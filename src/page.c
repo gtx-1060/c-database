9b92201f9a41701f40be7b9b2198a2d15653f6c9
Nenov Vladislav <klientkey200213@gmail.com>
2023-10-09 17:23:42 +0300

    set_into_bitmap method, paging handling methods fixes

--- src/page.c
+++ src/page.c
@@ -28,11 +28,12 @@ typedef enum RowReadStatus {
     READ_ROW_OK,
     READ_ROW_OUT_OF_BOUND,
     READ_ROW_IS_NULL,
-    DEST_ROW_NOT_FREE
+    DEST_NOT_FREE
 } RowReadStatus;
 
 typedef enum RowWriteResult {
     WRITE_ROW_OK,
+    WRITE_BITMAP_ERROR,
     WRITE_ROW_NOT_EMPTY,
     WRITE_ROW_OUT_OF_BOUND
 } RowWriteResult;
@@ -81,18 +82,35 @@ uint32_t first_row_offset(const PageMeta* header) {
     return 10+(uint32_t)ceil(((double)rows_number(header))/8);
 }
 
+// return -1, if the bit already has same value
+// return 0 if success
+int8_t set_into_bitmap(uint8_t* page, uint32_t index, uint8_t value) {
+    uint8_t* bitmap_pointer = page + 10 + (index/8);
+    uint8_t mask = (uint8_t)pow(2, (7-index) % 8);
+    uint8_t masked = mask & (*bitmap_pointer);
+    if ((masked != 0 && value != 0) || (masked == 0 && value == 0))
+        return -1;
+    if (value == 1) {
+        *bitmap_pointer = (*bitmap_pointer) | mask;
+    } else {
+        mask = ~mask;
+        *bitmap_pointer = (*bitmap_pointer) | mask;
+    }
+    return 0;
+}
+
 uint8_t is_row_null(const uint8_t* page, uint32_t row_index) {
     return (*(page + 10 + (row_index / 8)) << (row_index % 8)) & 0b10000000;
 }
 
 RowReadStatus read_row(MemoryManager* manager, PageMeta* header, uint32_t index, PageRow* dest) {
-    uint8_t* page = (uint8_t*)get_mapped_pages(manager, header->offset, 1);
+    uint8_t* page = (uint8_t*)get_mapped_pages(manager, header->offset, header->scale);
     if (index >= rows_number(header))
         return READ_ROW_OUT_OF_BOUND;
     if (is_row_null(page, index))
         return READ_ROW_IS_NULL;
     if (dest->data != NULL)
-        return DEST_ROW_NOT_FREE;
+        return DEST_NOT_FREE;
     dest->index = index;
     dest->size = header->row_size;
     dest->data = malloc(header->row_size);
@@ -115,21 +133,26 @@ RowWriteResult write_row(MemoryManager* manager, PageMeta* header, PageRow* row)
     if (!is_row_null(page, row->index)) {
         return WRITE_ROW_NOT_EMPTY;
     }
+    if(set_into_bitmap(page, row->index, 1) != 0 )
+        return WRITE_BITMAP_ERROR;
     memcpy(page+first_row_offset(header)+row->index, page, row->size);
-    
+    return WRITE_ROW_OK;
 }
 
-uint32_t find_empty_row(MemoryManager* manager, PageMeta* header) {
+int64_t find_empty_row(MemoryManager* manager, PageMeta* header) {
     uint8_t* page = (uint8_t*)get_mapped_pages(manager, header->offset, 1);
     uint32_t rows = rows_number(header);
     uint32_t i = 0;
     for (uint8_t* bitmap = page+10; bitmap < page+10+(rows/8); bitmap++) {
         uint8_t mask = 0b10000000;
+        uint8_t in_byte = 0;
         do {
             if (((*bitmap) & mask) != 0)
-                return i;
-            i++;
+                return i+in_byte;
+            in_byte++;
         } while ((mask >>= 1) != 0);
+        i += 8;
     }
+    return -1;
 }
 
